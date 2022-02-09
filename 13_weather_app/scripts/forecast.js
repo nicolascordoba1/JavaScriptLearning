c1b7e0ce0c49f128435a507195f23e00faadc236
Nicolas <nicolascordoba97@hotmail.com>
2022-02-08 23:08:45 -0500

    Se agregó funcionalidad con JS

---
+++ 13_weather_app/scripts/forecast.js
@@ -0,0 +1,27 @@
+const key = 'N0sFPKz7nBQmyDAGjpiGgz4vSylJKbFe'
+
+
+//Get weather information
+const getWeather =async(id) =>{
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+//get City Information
+const getCity = async(city) =>{
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const   query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
